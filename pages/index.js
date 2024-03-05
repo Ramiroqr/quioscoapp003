e4ef20a32cc7d00b6221fa7387e72394e0cae2b3
ramiroquisper@gmail.com <ramiroquisper@gmail.com>
2024-03-05 16:00:10 -0500

    Using getServerSideProps

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,23 @@
 import Image from "next/image";
-import { Inter } from "next/font/google";
+import { PrismaClient } from "@prisma/client";
 
-const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+export default function Home({ categorias }) {
+  console.log(categorias);
+
   return (
     <h1>Next.Js</h1>
   );
+}
+
+export const getServerSideProps = async () => {
+  const prisma = new PrismaClient();
+
+  const categorias = await prisma.categoria.findMany();
+
+  return {
+    props: {
+      categorias,
+    },
+  };
 }
